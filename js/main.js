90ed4aa2a9db260d8479acd6e246c27611d70c12
umbertofaenza <umberto.faenza@gmail.com>
2023-07-18 16:43:27 +0200

    feat: add backtick version

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ const userFavColor = prompt("Type your favourite colour here:");
 const generatedResult = userFirstName + userLastName + userFavColor + "21";
 
 document.getElementById("user-info").innerHTML = generatedResult;
+
+const backtickMessage =
+  "This paragraph and the text right below it are presented to you using backtick.";
+
+const generatedResultBT = `
+<p>${backtickMessage}</p>
+<h1>${generatedResult}</h1>
+`;
+
+document.getElementById("user-info-bt").innerHTML = generatedResultBT;
