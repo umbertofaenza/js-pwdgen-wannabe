b2074a7eca11348068bce5a24c6910213eaee1fd
umbertofaenza <umberto.faenza@gmail.com>
2023-07-18 15:33:27 +0200

    js_feat: add const for result

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 const userFirstName = prompt("Type your first name here:");
 const userLastName = prompt("Type your last name here:");
 const userFavColor = prompt("Type your favourite colour here:");
+
+const generatedResult = userFirstName + userLastName + userFavColor + "21";
